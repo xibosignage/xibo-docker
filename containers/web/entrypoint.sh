0d8d86786e5ee5b492a362859db19d2d725f3fd1
Alex Harrington <alex@springsignage.com>
2016-04-11 12:00:01 +0100

    Allow time for MySQL to start before starting the automatic provisioning

--- containers/web/entrypoint.sh
+++ containers/web/entrypoint.sh
@@ -51,13 +51,38 @@ then
   chown www-data.www-data -R /var/www/xibo/web
   chown www-data.www-data -R /var/www/xibo/install
   mkdir /var/www/xibo/cache
-  chown www-data.www-data -R /var/www/xibo/cache
+  mkdir /var/www/xibo/library
+  chown www-data.www-data -R /var/www/xibo/cache /var/www/xibo/library
   
-  if [! -e "/var/www/xibo/web/settings.php" ]
+  if [ ! -e "/var/www/xibo/web/settings.php" ]
   then
     # This is a fresh install so bootstrap the whole
     # system
     echo "New install"
+    
+    # Sleep for a few seconds to give MySQL time to initialise
+    echo "Waiting for MySQL to start - max 300 seconds"
+    /usr/local/bin/wait-for-it.sh -q -t 300 mysql:3306
+    
+    if [ ! "$?" == 0 ]
+    then
+      echo "MySQL didn't start in the allocated time" > /var/www/backup/LOG
+    fi
+    
+    # Safety sleep to give MySQL a moment to settle after coming up
+    sleep 1
+    
+    # Create database
+    MYSQL_USER_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+    mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql -e "CREATE DATABASE cms"
+    mysql -D cms -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql -e "SOURCE /var/www/xibo/install/master/structure.sql"
+    mysql -D cms -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql -e "SOURCE /var/www/xibo/install/master/data.sql"
+    mysql -D cms -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql -e "SOURCE /var/www/xibo/install/master/constraints.sql"
+    
+    mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h mysql -e "GRANT ALL PRIVILEGES ON cms.* TO 'cms_user'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD'; FLUSH PRIVILEGES;"
+    echo $MYSQL_USER_PASSWORD > /var/www/backup/.mysql-user-password
+    chmod 400 /var/www/backup/.mysql-user-password    
+        
     # Write settings.php
 
     # Set LIBRARY_LOCATION
